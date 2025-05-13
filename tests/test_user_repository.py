aca4458afca574d76efc20fbbaa951ee2038cda9
Paul Patterson <pazerp@hotmail.com>
2025-05-13 20:29:23 +0100

    Turned password in db into hashed equivalents.

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -52,8 +52,8 @@ def test_can_register_valid_email_address(db_connection):
     retval = user_repository.register_new_user('rupert@example.com', '12345678!')
     assert retval is None
 
-#### Registering a password
 
+#### Registering a password
 
 def test_cannot_register_password_containing_whitespace(db_connection):
     db_connection.seed("seeds/makersbnb.sql")
@@ -71,7 +71,6 @@ def test_cannot_register_password_without_special_character(db_connection):
     error_message = str(e.value)
     assert error_message == 'password must contain at least one of the following characters: ! @ £ $ % ^ & '
 
-
 def test_cannot_register_password_with_more_than_20_characters(db_connection):
     db_connection.seed("seeds/makersbnb.sql")
     user_repository = UserRepository(db_connection)
@@ -79,7 +78,6 @@ def test_cannot_register_password_with_more_than_20_characters(db_connection):
         _ = user_repository.register_new_user('gottfried@go_mail.com', '12345678!a12345678!a12345678!a')
     error_message = str(e.value)
     assert error_message == 'password must be less than 20 characters or less.'
-
 
 def test_cannot_register_password_less_than_8_characters(db_connection):
     db_connection.seed("seeds/makersbnb.sql")
@@ -94,27 +92,24 @@ def test_can_register_valid_password(db_connection):
     user_repository = UserRepository(db_connection)
     assert user_repository.register_new_user('gottfried@go_mail.com', '12345678!') is None
 
+#### Password checking
 
-# def test_can_register_valid_password(db_connection):
-#     db_connection.seed("seeds/makersbnb.sql")
-#     user_repository = UserRepository(db_connection)
-#     assert user_repository.check_password('puss@notmail.com', 'over_the_🌝')
-#     assert user_repository.check_password('fred_frost_02@gmail.com', '12345678!')
-#     assert user_repository.check_password('user1@hotmail.com', 'I 💜 🚗 and 🚜')
+def test_check_password_rejects_wrong_password(db_connection):
+    db_connection.seed("seeds/makersbnb.sql")
+    user_repository = UserRepository(db_connection)
+    assert not user_repository.check_password('alice@example.com', 'password123')
+    assert not user_repository.check_password('bob@example.com', '_@qwerty456')
+    assert not user_repository.check_password('carol@example.com', 'securepass789$$')
+    assert not user_repository.check_password('dave@example.com', '&&let&mei&n32')
+    assert not user_repository.check_password('eve@example.com', 'a@dmi£n123')
 
-#
+def test_check_password_accepts_correct_password(db_connection):
+    db_connection.seed("seeds/makersbnb.sql")
+    user_repository = UserRepository(db_connection)
+    assert user_repository.check_password('alice@example.com', 'password123!')
+    assert user_repository.check_password('bob@example.com', '_@qwerty456£')
+    assert user_repository.check_password('carol@example.com', 'securepass789$$^')
+    assert user_repository.check_password('dave@example.com', '&&let&mei&n321')
+    assert user_repository.check_password('eve@example.com', 'a@dmi£n1234')
 
-#
-# def test_register_password_too_short(db_connection):
 
-#
-# def test_register_password_lacks_special_character(db_connection):
-
-#
-#
-# def test_find_by_id_invalid(db_connection):
-#     db_connection.seed("seeds/login_form.sql")
-#     user_repository = UserRepository(db_connection)
-#     with pytest.raises(AssertionError) as e:
-#         user = user_repository.find_by_id(0)
-#     assert str(e.value) == f"id '0' not recognized."
