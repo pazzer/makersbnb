071e409b76c8e07fb19679987d2cdbc4580af728
Paul Patterson <pazerp@hotmail.com>
2025-05-14 15:43:47 +0100

    Added a couple of files from development.

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 from flask import Flask, request, render_template, redirect, session
+from lib.database_connection import get_flask_database_connection
 
 from lib.registration_values import RegistrationValues
 from lib.login_values import LoginValues
-from lib.database_connection import get_flask_database_connection
-from lib.user_repository import UserRepository
+
 from lib.custom_exceptions import MakersBnbException
+from lib.user_repository import UserRepository
+from lib.booking_repository import BookingRepository
+from lib.space_repository import SpaceRepository
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -13,14 +16,21 @@ app = Flask(__name__)
 # `session` admin
 app.secret_key = os.urandom(24)
 
+# GET /index
+# Returns the homepage
+# Try it:
+#   ; open http://localhost:5001/index
+@app.route('/index', methods=['GET'])
+def get_index():
+    return render_template('example_base_extended.html')
 
-# == Your Routes Here ==
+# The 'empty route' - redirect to register
 
 @app.route('/')
 def empty_route():
     return redirect('/register')
 
-# Registration
+# ------------------------------- Registration and login  ---------------------------------------------------
 
 @app.route('/register')
 def show_registration_form():
@@ -109,22 +119,96 @@ def log_out():
 
 # Spaces
 
-@app.route('/spaces')
-def show_spaces():
-    if 'user_id' in session:
-        db_conn = get_flask_database_connection(app)
-        user_repository = UserRepository(db_conn)
-        user = user_repository.find_by_id(session['user_id'])
-        return render_template('spaces.html', user=user, dummy_space_strings=[
-            '🏰 Cozy Cabin',
-            '🏠 Urban Loft',
-            '🛖 Beach Bungalow',
-            '⛪️ Mountain Retreat',
-            '🏥 Modern Studio'
-        ])
-    else:
-        return redirect('/login')
+# @app.route('/spaces')
+# def show_spaces():
+#     if 'user_id' in session:
+#         db_conn = get_flask_database_connection(app)
+#         user_repository = UserRepository(db_conn)
+#         user = user_repository.find_by_id(session['user_id'])
+#         return render_template('spaces.html', user=user, dummy_space_strings=[
+#             '🏰 Cozy Cabin',
+#             '🏠 Urban Loft',
+#             '🛖 Beach Bungalow',
+#             '⛪️ Mountain Retreat',
+#             '🏥 Modern Studio'
+#         ])
+#     else:
+#         return redirect('/login')
 
+
+
+
+# ------------------------ manage spaces page ---------------------------------------------
+
+@app.route('/myspaces/manage', methods=['GET'])
+def space_manager():
+    user_id = session.get('user_id', None)
+    if user_id is not None:
+        connection = get_flask_database_connection(app)
+        repository = SpaceRepository(connection)
+        spaces = repository.find_for_user(user_id)
+        return render_template('space_manager.html', spaces=spaces)
+    else:
+        return redirect(f'/login')
+
+# ------------------------ bookings routes ------------------------------------------------------
+
+# GET /myspaces/bookings/<space_id>
+# Returns confirmed bookings for a space
+@app.route('/myspaces/bookings/<int:space_id>', methods=['GET'])
+def get_bookings(space_id):
+    connection = get_flask_database_connection(app)
+    repository = BookingRepository(connection)
+    bookings = repository.view_bookings(space_id)
+    return render_template('myspaces_bookings.html', bookings=bookings)
+
+# GET /myspaces/requests/<space_id>
+# Returns requests to book for a space
+@app.route('/myspaces/requests/<int:space_id>', methods=['GET'])
+def get_requests(space_id):
+    connection = get_flask_database_connection(app)
+    repository = BookingRepository(connection)
+    requests = repository.view_requests(space_id)
+    return render_template('myspaces_requests.html', requests=requests)
+
+#  POST (DELETE) myspaces/requests/<space_id>/<booking_id>/reject
+# Deletes a request when it is rejected
+@app.route('/myspaces/requests/<int:space_id>/<int:booking_id>/reject', methods=['POST'])
+def delete_request(booking_id, space_id):
+    connection = get_flask_database_connection(app)
+    repository = BookingRepository(connection)
+    repository.reject_request(booking_id)
+    return redirect(f'/myspaces/requests/{space_id}')
+
+# POST (PUT) myspaces/requests/<space_id>/<booking_id>/accept
+# Accepts a booking request and changes is_confirmed to true
+@app.route('/myspaces/requests/<int:space_id>/<int:booking_id>/accept', methods=['POST'])
+def accept_request(booking_id, space_id):
+    connection = get_flask_database_connection(app)
+    repository = BookingRepository(connection)
+    repository.approve_request(booking_id)
+    return redirect(f'/myspaces/requests/{space_id}')
+
+# -------------------------------------------- Spaces routes ---------------------------------------------------
+
+# GET / spaces
+# Shows user all spaces listed on our website as soon as they log in
+@app.route('/spaces', methods=['GET'])
+def get_all_spaces():
+    session['user_id'] = 1
+    connection = get_flask_database_connection(app)
+    repository  = SpaceRepository(connection)
+    spaces = repository.list_spaces()
+    return render_template('spaces_all.html', spaces=spaces)
+
+# GET / spaces/<int:space_id>
+# Shows user an individual space when they click a button to view more info or book
+@app.route('/spaces/<int:space_id>', methods=['GET'])
+def get_individual_space(space_id):
+    connection = get_flask_database_connection(app)
+    repository = SpaceRepository(connection)
+    space = repository.find(space_id)
+    return render_template('space_individual.html', space=space)
 
 
 # These lines start the server if you run this file directly
--- seeds/makersbnb.sql
+++ seeds/makersbnb.sql
@@ -67,7 +67,7 @@ CREATE TABLE spaces (
     space_id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     description VARCHAR(255),
-    price_per_night VARCHAR(255),
+    price_per_night INT,
     user_id INT,
         CONSTRAINT fk_users FOREIGN KEY(user_id) REFERENCES users(user_id)
         ON DELETE CASCADE
@@ -75,11 +75,11 @@ CREATE TABLE spaces (
 
 -- Add any records that are needed for the tests to run
 INSERT INTO spaces (name, description, price_per_night, user_id) VALUES
-('Cozy Cabin', 'Rustic cabin in the forest.', '100', 1),
-('Urban Loft', 'Sleek apartment in downtown.', '150', 2),
-('Beach Bungalow', 'Sunny spot by the sea.', '200', 3),
-('Mountain Retreat', 'Quiet escape in the hills.', '180', 4),
-('Modern Studio', 'Compact yet luxurious.', '120', 5);
+('Cozy Cabin', 'Rustic cabin in the forest.', 100, 1),
+('Urban Loft', 'Sleek apartment in downtown.', 150, 2),
+('Beach Bungalow', 'Sunny spot by the sea.', 200, 3),
+('Mountain Retreat', 'Quiet escape in the hills.', 180, 4),
+('Modern Studio', 'Compact yet luxurious.', 120, 5);
 
 ------------------------ availabileRanges ------------------------
 
@@ -129,8 +129,12 @@ CREATE TABLE bookings (
 
 -- Add any records that are needed for the tests to run
 INSERT INTO bookings (start_range, end_range, space_id, user_id, is_confirmed) VALUES
-('2025-06-02', '2025-06-05', 1, 2, FALSE), -- 'Crazy Cabin,
-('2025-06-06', '2025-06-09', 2, 3, FALSE), -- 'Urban Loft'
-('2025-07-02', '2025-07-04', 3, 4, FALSE), -- 'Beach Bungalow'
-('2025-07-16', '2025-07-20', 4, 5, FALSE), -- 'Mountain Retreat'
-('2025-08-02', '2025-08-06', 5, 1, FALSE); -- 'Modern Studio'
+('2025-06-02', '2025-06-05', 1, 2, FALSE),
+('2025-06-06', '2025-06-09', 2, 3, FALSE),
+('2025-07-02', '2025-07-04', 3, 4, FALSE),
+('2025-07-16', '2025-07-20', 4, 5, FALSE),
+('2025-08-02', '2025-08-06', 5, 1, FALSE),
+('2025-08-04', '2025-08-011', 5, 2, TRUE),
+('2025-06-17', '2025-06-22', 5, 2, TRUE),
+('2025-07-02', '2025-07-06', 5, 5, TRUE),
+('2025-07-02', '2025-07-06', 5, 5, FALSE);
