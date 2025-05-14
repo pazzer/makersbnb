2daaf4bae61e3586035fa06ffe023793b93743ac
zehad <zehad.tech@gmail.com>
2025-05-14 17:18:15 +0100

    Zehad & Paul pushing list_spaces_by_date_range

--- lib/space_repository.py
+++ lib/space_repository.py
@@ -1,4 +1,5 @@
 from lib.space import Space
+from lib.available_range import *
 
 class SpaceRepository:
     def __init__(self, _connection):
@@ -12,6 +13,21 @@ class SpaceRepository:
             item = Space(row['space_id'], row['name'], row['description'], row['price_per_night'], row['user_id'])
             spaces.append(item)
         return spaces
+
+    def list_spaces_by_date_range(self, holiday_start, holiday_end):
+        '''
+        1) need to check that the supplied dates fall within the available range for a property [done]
+        2) take our results from the first filter and look at every booking and check that our date range doesen't overlap 
+        '''
+
+        available_spaces = []
+        for space in self.list_spaces():
+            rows = self._connection.execute("SELECT * FROM availabileRanges WHERE space_id = %s ", [space.space_id])
+            available_range = AvailableRange.from_rowdict(rows[0])
+            if available_range.contains(holiday_start, holiday_end):
+                available_spaces.append(space)
+        return available_spaces
+
 
     def add_space(self, space):
         self._connection.execute(
